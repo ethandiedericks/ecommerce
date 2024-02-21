832d4481c1ab7e004ccb5a0c35d45cda94e99ed4
Ethan Diedericks <diedericksethan@gmail.com>
2024-02-21 18:23:06 +0200

    configured image uploads

--- backend/api/models.py
+++ backend/api/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     stock_level = models.IntegerField(default=0)
+    image = models.ImageField(upload_to="product_images/")
 
     def __str__(self):
         return self.name
