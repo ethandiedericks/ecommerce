45a495afd00317f90fb50ec650f96bb58c7dea0d
Ethan Diedericks <diedericksethan@gmail.com>
2024-02-24 19:28:41 +0200

    ui changes

--- frontend/ecommerce/src/components/Header/MobileNav.jsx
+++ frontend/ecommerce/src/components/Header/MobileNav.jsx
@@ -1,5 +1,6 @@
 import { Dialog } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
+
 const MobileNav = ({ mobileMenuOpen, setMobileMenuOpen }) => (
     <Dialog as="div" className="md:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-50" />
@@ -24,26 +25,7 @@ const MobileNav = ({ mobileMenuOpen, setMobileMenuOpen }) => (
                     </div>
                     <div className="mt-6 flow-root">
                         <div className="-my-6 divide-y divide-gray-500/10">
-                            <div className="space-y-2 py-6">
-                                <form className='m-0'>   
-                                    <label htmlFor="search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                                    <div className="relative">
-                                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                            </svg>
-                                        </div>
-                                        <input 
-                                            type="search" 
-                                            id="search" 
-                                            className="block w-full h-10 p-4 ps-10 text-sm text-gray-900 rounded-lg bg-gray-100 dark:bg-gray-700 dark:placeholder-gray-400 dark:text-white" 
-                                            placeholder="Search" 
-                                            required 
-                                        />
-                                        
-                                    </div>
-                                </form>
-                            </div>
+            
                             <div className="py-6">
                                 <a
                                     href="#"
--- frontend/ecommerce/src/components/Hero/Hero.jsx
+++ frontend/ecommerce/src/components/Hero/Hero.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-
 const Hero = () => {
   return (
         <div className="relative overflow-hidden">
             <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
                 <div className="text-center">
                 <h1 className="text-4xl sm:text-6xl font-bold text-gray-800 dark:text-gray-200">
-                    Insights
+                    Ecommerce
                 </h1>
 
                 <p className="mt-3 text-gray-600 dark:text-gray-400">
-                    Stay in the know with insights from industry experts.
+                Empower Your Style, Elevate Your Experience. Welcome to Your Ultimate Shopping Destination.
                 </p>
 
                 <div className="mt-7 sm:mt-12 mx-auto max-w-xl relative">
@@ -18,8 +17,7 @@ const Hero = () => {
                     <form>
                     <div className="relative z-10 flex space-x-3 p-3 bg-white border rounded-lg shadow-lg shadow-gray-100 dark:bg-slate-900 dark:border-gray-700 dark:shadow-gray-900/[.2]">
                         <div className="flex-[1_0_0%]">
-                        <label htmlFor="hs-search-article-1" className="block text-sm text-gray-700 font-medium dark:text-white"><span className="sr-only">Search article</span></label>
-                        <input type="email" name="hs-search-article-1" id="hs-search-article-1" className="py-2.5 px-4 block w-full border-transparent rounded-lg focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Search article"/>
+                        <input type="email" name="hs-search-article-1" id="hs-search-article-1" className="py-2.5 px-4 block w-full border-transparent rounded-lg focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-transparent dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Search for products, brands..."/>
                         </div>
                         <div className="flex-[0_0_auto]">
                         <a className="size-[46px] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
