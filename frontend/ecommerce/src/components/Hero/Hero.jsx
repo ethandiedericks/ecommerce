8456809c25c69142291cd438de59c10c3fadd824
Ethan Diedericks <diedericksethan@gmail.com>
2024-02-26 18:31:28 +0200

    ui changes

--- frontend/ecommerce/src/components/Hero/Hero.jsx
+++ frontend/ecommerce/src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
             <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 sm:py-24">
                 <div className="text-center">
                 <h1 className="text-4xl sm:text-6xl font-bold text-gray-800 dark:text-gray-200">
-                    Ecommerce
+                    e-commerce
                 </h1>
 
                 <p className="mt-3 text-gray-600 dark:text-gray-400">
