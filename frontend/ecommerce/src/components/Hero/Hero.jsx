d68ab855e00e100f595c76ddc015d3b8e4484221
Ethan Diedericks <diedericksethan@gmail.com>
2024-03-05 03:36:04 +0200

    hero search adjusted

--- frontend/ecommerce/src/components/Hero/Hero.jsx
+++ frontend/ecommerce/src/components/Hero/Hero.jsx
@@ -16,18 +16,18 @@ const Hero = () => {
                 <div className="mt-7 sm:mt-12 mx-auto max-w-xl relative">
                     {/* Form */}
                     <form>
-                    <div className="relative z-10 flex space-x-3 p-3 bg-white border rounded-lg shadow-lg shadow-gray-100 dark:bg-slate-900 dark:border-gray-700 dark:shadow-gray-900/[.2]">
+                    <div className="relative z-10 flex space-x-3 p-3 bg-white border rounded-3xl shadow-lg shadow-gray-100 dark:bg-slate-900 dark:border-gray-700 dark:shadow-gray-900/[.2]">
                         <div className="flex-[1_0_0%]">
                         <input 
                             type="search" 
                             name="hs-search-article-1" 
                             id="hs-search-article-1" 
-                            className="py-2.5 px-4 block w-full border-transparent rounded-lg dark:bg-slate-900 dark:border-transparent dark:text-gray-400" 
+                            className="py-2.5 px-4 block w-full border-transparent rounded-3xl dark:bg-slate-900 dark:border-transparent dark:text-gray-400" 
                             placeholder="Search for products, brands..."
                         />
                         </div>
                         <div className="flex-[0_0_auto]">
-                        <a className="size-[46px] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+                        <a className="size-[46px] inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-3xl border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                             </svg>
